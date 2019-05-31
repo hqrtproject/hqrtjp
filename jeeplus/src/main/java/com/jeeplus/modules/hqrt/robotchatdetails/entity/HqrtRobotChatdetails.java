695866b9254cb5a2bd8b942eff8b7d57ffc68cdd
czcys <158329057@qq.com>
2019-05-31 20:32:02 +0800

    补充

---
+++ jeeplus/src/main/java/com/jeeplus/modules/hqrt/robotchatdetails/entity/HqrtRobotChatdetails.java
@@ -0,0 +1,305 @@
+/**
+ * Copyright &copy; 2015-2020 <a href="http://www.jeeplus.org/">JeePlus</a> All rights reserved.
+ */
+package com.jeeplus.modules.hqrt.robotchatdetails.entity;
+
+
+import java.util.List;
+
+import com.jeeplus.common.utils.excel.annotation.ExcelField;
+import com.jeeplus.core.persistence.DataEntity;
+import com.jeeplus.modules.hqrt.robotchat.entity.HqrtRobotChat;
+
+/**
+ * 机器人对话内容Entity
+ * @author cz
+ * @version 2019-05-16
+ */
+public class HqrtRobotChatdetails extends DataEntity<HqrtRobotChatdetails> {
+	
+	private static final long serialVersionUID = 1L;
+	private String rowguid;		// 唯一记录标识（主键）
+	private String rowdatetime;		// 记录创建时间
+	private String sessionid;		// 会话ID
+	private String customerid;		// 客户标识
+	private String customername;		// 客户姓名
+	private String customermobile;		// 客户手机号码
+	private String customerprovince;		// 客户所在省
+	private String queueid;		// 队列（技能组）编号
+	private String queuename;		// 队列（技能组）名称
+	private List<String> queuenameList;	// 选中业务系统（可能是多个）
+	private String requestcontext;		// 客户发起咨询的内容
+	private String responsecontext;		// 机器人回复的内容
+	private Integer responseno;		// 机器人回复编码:100无回复；200回复知识;300回复推荐
+	private String responsenodesc;		// 机器人回复编码描述:100无回复；200回复知识;300回复推荐
+	private String faqid;		// 对应的是知识库中t_faq表主键tuid
+	private String faqroot;		// 知识根目录（所处业务系统）
+	private String faqmodel;		// 知识子模块
+	private String faqserialno;		// 知识序列号
+	private String faqtitle;		// 知识标题
+	private String faqcreaterid;		// 知识创建人标识
+	private String faqcreatername;		// 知识创建人姓名
+	private String faqcreatedatetime;		// 知识创建时间
+	private Integer satisfyno;		// 是否解决了客户的问题(0未评价；1未解决；2已解决)
+	private String satisfydesc;		// 是否解决了客户的问题描述
+	private String messagedatetime;		// 消息时间
+	private String originalsessionid;  // 原始SessionID
+	private HqrtRobotChat parent ;
+	private Integer ordernumber; // 序号
+	public HqrtRobotChatdetails() {
+		super();
+		this.setIdType(IDTYPE_AUTO);
+	}
+
+	public HqrtRobotChatdetails(String id){
+		super(id);
+	}
+
+	
+	public String getRowguid() {
+		return rowguid;
+	}
+
+	public void setRowguid(String rowguid) {
+		this.rowguid = rowguid;
+	}
+	
+	@ExcelField(title="用户提问时间", align=2, sort=10)
+	public String getRowdatetime() {
+		return rowdatetime;
+	}
+
+	public void setRowdatetime(String rowdatetime) {
+		this.rowdatetime = rowdatetime;
+	}
+	
+	@ExcelField(title="会话ID", align=2, sort=13)
+	public String getSessionid() {
+		return sessionid;
+	}
+
+	public void setSessionid(String sessionid) {
+		this.sessionid = sessionid;
+	}
+	
+	@ExcelField(title="用户ID", align=2, sort=8)
+	public String getCustomerid() {
+		return customerid;
+	}
+
+	public void setCustomerid(String customerid) {
+		this.customerid = customerid;
+	}
+	
+	@ExcelField(title="用户名", align=2, sort=7)
+	public String getCustomername() {
+		return customername;
+	}
+
+	public void setCustomername(String customername) {
+		this.customername = customername;
+	}
+	
+	
+	public String getCustomermobile() {
+		return customermobile;
+	}
+
+	public void setCustomermobile(String customermobile) {
+		this.customermobile = customermobile;
+	}
+	
+	@ExcelField(title="用户所属省份", align=2, sort=9)
+	public String getCustomerprovince() {
+		return customerprovince;
+	}
+
+	public void setCustomerprovince(String customerprovince) {
+		this.customerprovince = customerprovince;
+	}
+	
+	
+	public String getQueueid() {
+		return queueid;
+	}
+
+	public void setQueueid(String queueid) {
+		this.queueid = queueid;
+	}
+	
+	@ExcelField(title="业务系统", align=2, sort=2)
+	public String getQueuename() {
+		return queuename;
+	}
+
+	public void setQueuename(String queuename) {
+		this.queuename = queuename;
+	}
+	
+	public List<String> getQueuenameList() {
+		return queuenameList;
+	}
+
+	public void setQueuenameList(List<String> queuenameList) {
+		this.queuenameList = queuenameList;
+	}
+
+	@ExcelField(title="用户提问内容", align=2, sort=11)
+	public String getRequestcontext() {
+		return requestcontext;
+	}
+
+	public void setRequestcontext(String requestcontext) {
+		this.requestcontext = requestcontext;
+	}
+	
+	
+	public String getResponsecontext() {
+		return responsecontext;
+	}
+
+	public void setResponsecontext(String responsecontext) {
+		this.responsecontext = responsecontext;
+	}
+	
+	
+	public Integer getResponseno() {
+		return responseno;
+	}
+
+	public void setResponseno(Integer responseno) {
+		this.responseno = responseno;
+	}
+	
+	
+	public String getResponsenodesc() {
+		return responsenodesc;
+	}
+
+	public void setResponsenodesc(String responsenodesc) {
+		this.responsenodesc = responsenodesc;
+	}
+	
+	
+	public String getFaqid() {
+		return faqid;
+	}
+
+	public void setFaqid(String faqid) {
+		this.faqid = faqid;
+	}
+	
+	@ExcelField(title="知识分类", align=2, sort=3)
+	public String getFaqroot() {
+		return faqroot;
+	}
+
+	public void setFaqroot(String faqroot) {
+		this.faqroot = faqroot;
+	}
+	
+	@ExcelField(title="子模块", align=2, sort=4)
+	public String getFaqmodel() {
+		return faqmodel;
+	}
+
+	public void setFaqmodel(String faqmodel) {
+		this.faqmodel = faqmodel;
+	}
+	
+	@ExcelField(title="知识编码", align=2, sort=5)
+	public String getFaqserialno() {
+		return faqserialno;
+	}
+
+	public void setFaqserialno(String faqserialno) {
+		this.faqserialno = faqserialno;
+	}
+	
+	@ExcelField(title="知识标题", align=2, sort=6)
+	public String getFaqtitle() {
+		return faqtitle;
+	}
+
+	public void setFaqtitle(String faqtitle) {
+		this.faqtitle = faqtitle;
+	}
+	
+	@ExcelField(title="知识创建人", align=2, sort=14)
+	public String getFaqcreaterid() {
+		return faqcreaterid;
+	}
+
+	public void setFaqcreaterid(String faqcreaterid) {
+		this.faqcreaterid = faqcreaterid;
+	}
+	
+	public String getFaqcreatername() {
+		return faqcreatername;
+	}
+
+	public void setFaqcreatername(String faqcreatername) {
+		this.faqcreatername = faqcreatername;
+	}
+	
+	@ExcelField(title="知识创建时间", align=2, sort=15)
+	public String getFaqcreatedatetime() {
+		return faqcreatedatetime;
+	}
+
+	public void setFaqcreatedatetime(String faqcreatedatetime) {
+		this.faqcreatedatetime = faqcreatedatetime;
+	}
+	
+	
+	public Integer getSatisfyno() {
+		return satisfyno;
+	}
+
+	public void setSatisfyno(Integer satisfyno) {
+		this.satisfyno = satisfyno;
+	}
+	
+	@ExcelField(title="是否解决（解决状态）", align=2, sort=12)
+	public String getSatisfydesc() {
+		return satisfydesc;
+	}
+	public void setSatisfydesc(String satisfydesc) {
+		this.satisfydesc = satisfydesc;
+	}
+	
+	public String getMessagedatetime() {
+		return messagedatetime;
+	}
+
+	public void setMessagedatetime(String messagedatetime) {
+		this.messagedatetime = messagedatetime;
+	}
+	
+	
+	public String getOriginalsessionid() {
+		return originalsessionid;
+	}
+
+	public void setOriginalsessionid(String originalsessionid) {
+		this.originalsessionid = originalsessionid;
+	}
+
+	public HqrtRobotChat getParent() {
+		return parent;
+	}
+
+	public void setParent(HqrtRobotChat parent) {
+		this.parent = parent;
+	}
+	
+	@ExcelField(title="序号", align=2, sort=1)
+	public Integer getOrdernumber() {
+		return ordernumber;
+	}
+
+	public void setOrdernumber(Integer ordernumber) {
+		this.ordernumber = ordernumber;
+	}
+
+}
---
+++ jeeplus/src/main/java/com/jeeplus/modules/hqrt/robotchatdetails/web/HqrtRobotChatdetailsController.java
@@ -0,0 +1,251 @@
+/**
+ * Copyright &copy; 2015-2020 <a href="http://www.jeeplus.org/">JeePlus</a> All rights reserved.
+ */
+package com.jeeplus.modules.hqrt.robotchatdetails.web;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jeeplus.common.config.Global;
+import com.jeeplus.common.json.AjaxJson;
+import com.jeeplus.common.utils.DateUtils;
+import com.jeeplus.common.utils.StringUtils;
+import com.jeeplus.common.utils.excel.ExportExcel;
+import com.jeeplus.core.persistence.Page;
+import com.jeeplus.core.service.BaseService;
+import com.jeeplus.core.web.BaseController;
+import com.jeeplus.modules.hqrt.queueconfig.entity.HqrtQueueConfig;
+import com.jeeplus.modules.hqrt.robotchatdetails.entity.HqrtRobotChatdetails;
+import com.jeeplus.modules.hqrt.robotchatdetails.service.HqrtRobotChatdetailsService;
+import com.jeeplus.modules.tools.utils.MultiDBUtils;
+
+/**
+ * 机器人对话内容Controller
+ * @author cz
+ * @version 2019-05-16
+ */
+@Controller
+@RequestMapping(value = "${adminPath}/hqrt/robotchatdetails/hqrtRobotChatdetails")
+public class HqrtRobotChatdetailsController extends BaseController {
+
+	@Autowired
+	private HqrtRobotChatdetailsService hqrtRobotChatdetailsService;
+	
+	@ModelAttribute
+	public HqrtRobotChatdetails get(@RequestParam(required=false) String id) {
+		HqrtRobotChatdetails entity = null;
+		if (StringUtils.isNotBlank(id)){
+			entity = hqrtRobotChatdetailsService.get(id);
+		}
+		if (entity == null){
+			entity = new HqrtRobotChatdetails();
+		}
+		return entity;
+	}
+	
+	/**
+	 * 机器人对话内容列表页面
+	 */
+	@RequestMapping(value = {"list", ""})
+	public String list(
+			/*@RequestParam(name="timestamp", required=true) Long timestamp,
+			@RequestParam(name="sign", required=true) String sign,*/
+			HqrtRobotChatdetails hqrtRobotChatdetails, Model model) {
+		/*long nowtimestamp = System.currentTimeMillis();
+		Long ratio = Math.round(nowtimestamp*0.1/600000*10);
+		if (ratio.equals(timestamp)) {
+			String salt = ratio + Global.getConfig("secretkey");
+			String md5 = DigestUtils.md5Hex(salt);
+			if (md5.equals(sign)) {
+				model.addAttribute("hqrtRobotChatdetails", hqrtRobotChatdetails);
+				return "modules/hqrt/robotchatdetails/hqrtRobotChatdetailsList";
+			} else {
+				return "modules/hqrt/404";
+			}
+		} else {
+			return "modules/hqrt/404";
+		}*/
+		model.addAttribute("hqrtRobotChatdetails", hqrtRobotChatdetails);
+		return "modules/hqrt/robotchatdetails/hqrtRobotChatdetailsList";
+	}
+	
+		/**
+	 * 机器人对话内容列表数据
+	 */
+	@ResponseBody
+	@RequestMapping(value = "data")
+	public Map<String, Object> data(HqrtRobotChatdetails hqrtRobotChatdetails, HttpServletRequest request, HttpServletResponse response, Model model) {
+		String sql = "select a.id AS 'id',a.rowguid AS 'rowguid',a.rowdatetime AS 'rowdatetime',a.sessionid AS 'sessionid',a.customerid AS 'customerid',a.customername AS 'customername',a.customermobile AS 'customermobile',a.customerprovince AS 'customerprovince',a.queueid AS 'queueid',a.queuename AS 'queuename', a.requestcontext AS 'requestcontext',a.responsecontext AS 'responsecontext', a.responseno AS 'responseno', a.responsenodesc AS 'responsenodesc',a.faqid AS 'faqid',a.faqroot AS 'faqroot',a.faqmodel AS 'faqmodel', a.faqserialno AS 'faqserialno',a.faqtitle AS 'faqtitle',a.faqcreaterid AS 'faqcreaterid',a.faqcreatername AS 'faqcreatername',a.faqcreatedatetime AS 'faqcreatedatetime',a.satisfyno AS 'satisfyno',a.satisfydesc AS 'satisfydesc',a.MessageDateTime  AS 'messagedatetime',a.originalsessionid AS 'originalsessionid' FROM hqrt_robot_chatdetails a ";
+        String sqlcondition = "";
+        List<Object> paramList = new ArrayList<Object>();
+        if (StringUtils.isNotBlank(hqrtRobotChatdetails.getQueuename())) {
+        	MultiDBUtils md = MultiDBUtils.get(Global.getConfig("datasourcename"));
+    		List<HqrtQueueConfig> hqrtQueueConfigList = md.queryList("SELECT a.QueueName FROM hqrt_queue_config a", HqrtQueueConfig.class);
+    		List<String> queueNameList = new ArrayList<String>();
+    		for (HqrtQueueConfig hqrtQueueConfig : hqrtQueueConfigList) {
+    			queueNameList.add(hqrtQueueConfig.getQueuename());
+    		}
+        	if (!hqrtRobotChatdetails.getQueuename().contains("其他")) {
+        		sqlcondition += " AND a.queuename in ('" + hqrtRobotChatdetails.getQueuename().replace(",", "','") + "')";
+			} else {
+				String[] queueselect = hqrtRobotChatdetails.getQueuename().split(",");
+				for (String queuename : queueselect) {
+					if (queueNameList.contains(queuename)) {
+						queueNameList.remove(queuename);
+					}
+				}
+				sqlcondition += " AND a.queuename not in ('" + StringUtils.join(queueNameList.toArray(), "','") + "')";
+			}
+        }
+        if (StringUtils.isNotBlank(hqrtRobotChatdetails.getSatisfydesc())) {
+        	sqlcondition += " AND a.satisfydesc = ?";
+        	paramList.add(hqrtRobotChatdetails.getSatisfydesc());
+        }
+        if (StringUtils.isNotBlank(hqrtRobotChatdetails.getSessionid())) {
+        	sqlcondition += " AND a.sessionid = ?";
+        	paramList.add(hqrtRobotChatdetails.getSessionid());
+        }
+        if (StringUtils.isNotBlank(hqrtRobotChatdetails.getCustomername())) {
+        	sqlcondition += " AND a.customername = ?";
+        	paramList.add(hqrtRobotChatdetails.getCustomername());
+        }
+        if (hqrtRobotChatdetails.getParent() != null && hqrtRobotChatdetails.getParent().getStarttime() != null && hqrtRobotChatdetails.getParent().getEndttime() != null) {
+        	sqlcondition += " AND a.messagedatetime BETWEEN ? AND ?";
+        	SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        	paramList.add(ft.format(hqrtRobotChatdetails.getParent().getStarttime()));
+        	paramList.add(ft.format(hqrtRobotChatdetails.getParent().getEndttime()));
+        } else {
+        	sqlcondition += " AND a.messagedatetime BETWEEN ? AND ?";
+        	SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        	Calendar cal = Calendar.getInstance();
+            cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+            Date beginOfDate = cal.getTime();
+        	paramList.add(ft.format(beginOfDate));
+        	Calendar calendar2 = Calendar.getInstance();
+        	calendar2.set(calendar2.get(Calendar.YEAR), calendar2.get(Calendar.MONTH), calendar2.get(Calendar.DAY_OF_MONTH),
+        	        23, 59, 59);
+        	Date endOfDate = calendar2.getTime();
+        	paramList.add(ft.format(endOfDate));
+        	
+        }
+        Page<HqrtRobotChatdetails> page = new Page<HqrtRobotChatdetails>(request, response);
+        if (StringUtils.isNotBlank(sqlcondition)) {
+        	sqlcondition = sqlcondition.replaceFirst(" AND", "");
+        	sqlcondition  = " where" + sqlcondition;
+        }
+        // 该语句仅仅为了查询当前条件下有多少符合条件的数据
+        String selectcountsql = sql + sqlcondition;
+        sql += sqlcondition + "limit " + (page.getPageNo()-1)*page.getPageSize() + "," + page.getPageSize();
+        MultiDBUtils md = MultiDBUtils.get(Global.getConfig("datasourcename"));
+        List<HqrtRobotChatdetails> detailsList = md.queryList(sql, HqrtRobotChatdetails.class, paramList.toArray());
+        List<HqrtRobotChatdetails> allDetailslList = md.queryList(selectcountsql, HqrtRobotChatdetails.class, paramList.toArray());
+        page.setCount(allDetailslList.size());
+		BaseService.dataRuleFilter(hqrtRobotChatdetails);
+		hqrtRobotChatdetails.setPage(page);
+		for (int i = 0; i < detailsList.size(); i++) {
+			detailsList.get(i).setOrdernumber(i+1+((page.getPageNo()-1)*page.getPageSize()));
+		}
+		page.setList(detailsList);
+		return getBootstrapData(page);
+	}
+
+	/**
+	 * 导出excel文件
+	 */
+	@ResponseBody
+    @RequestMapping(value = "export")
+    public AjaxJson exportFile(HqrtRobotChatdetails hqrtRobotChatdetails, HttpServletRequest request, HttpServletResponse response) {
+		AjaxJson j = new AjaxJson();
+		try {
+            String fileName = "机器人拦截明细日志"+DateUtils.getDate("yyyyMMddHHmmss")+".xlsx";
+            String sql = "select a.id AS 'id',a.rowguid AS 'rowguid',a.rowdatetime AS 'rowdatetime',a.sessionid AS 'sessionid',a.customerid AS 'customerid',a.customername AS 'customername',a.customermobile AS 'customermobile',a.customerprovince AS 'customerprovince',a.queueid AS 'queueid',a.queuename AS 'queuename', a.requestcontext AS 'requestcontext',a.responsecontext AS 'responsecontext', a.responseno AS 'responseno', a.responsenodesc AS 'responsenodesc',a.faqid AS 'faqid',a.faqroot AS 'faqroot',a.faqmodel AS 'faqmodel', a.faqserialno AS 'faqserialno',a.faqtitle AS 'faqtitle',a.faqcreaterid AS 'faqcreaterid',a.faqcreatername AS 'faqcreatername',a.faqcreatedatetime AS 'faqcreatedatetime',a.satisfyno AS 'satisfyno',a.satisfydesc AS 'satisfydesc',a.MessageDateTime  AS 'messagedatetime',a.originalsessionid AS 'originalsessionid' FROM hqrt_robot_chatdetails a ";
+            String sqlcondition = "";
+            List<Object> paramList = new ArrayList<Object>();
+            if (StringUtils.isNotBlank(hqrtRobotChatdetails.getQueuename())) {
+            	MultiDBUtils md = MultiDBUtils.get(Global.getConfig("datasourcename"));
+        		List<HqrtQueueConfig> hqrtQueueConfigList = md.queryList("SELECT a.QueueName FROM hqrt_queue_config a", HqrtQueueConfig.class);
+        		List<String> queueNameList = new ArrayList<String>();
+        		for (HqrtQueueConfig hqrtQueueConfig : hqrtQueueConfigList) {
+        			queueNameList.add(hqrtQueueConfig.getQueuename());
+        		}
+            	if (!hqrtRobotChatdetails.getQueuename().contains("其他")) {
+            		sqlcondition += " AND a.queuename in ('" + hqrtRobotChatdetails.getQueuename().replace(",", "','") + "')";
+    			} else {
+    				String[] queueselect = hqrtRobotChatdetails.getQueuename().split(",");
+    				for (String queuename : queueselect) {
+    					if (queueNameList.contains(queuename)) {
+    						queueNameList.remove(queuename);
+    					}
+    				}
+    				sqlcondition += " AND a.queuename not in ('" + StringUtils.join(queueNameList.toArray(), "','") + "')";
+    			}
+            }
+            if (StringUtils.isNotBlank(hqrtRobotChatdetails.getSatisfydesc())) {
+            	sqlcondition += " AND a.satisfydesc = ?";
+            	paramList.add(hqrtRobotChatdetails.getSatisfydesc());
+            }
+            if (StringUtils.isNotBlank(hqrtRobotChatdetails.getSessionid())) {
+            	sqlcondition += " AND a.sessionid = ?";
+            	paramList.add(hqrtRobotChatdetails.getSessionid());
+            }
+            if (StringUtils.isNotBlank(hqrtRobotChatdetails.getCustomername())) {
+            	sqlcondition += " AND a.customername = ?";
+            	paramList.add(hqrtRobotChatdetails.getCustomername());
+            }
+            if (hqrtRobotChatdetails.getParent() != null && hqrtRobotChatdetails.getParent().getStarttime() != null && hqrtRobotChatdetails.getParent().getEndttime() != null) {
+            	sqlcondition += " AND a.messagedatetime BETWEEN ? AND ?";
+            	SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            	paramList.add(ft.format(hqrtRobotChatdetails.getParent().getStarttime()));
+            	paramList.add(ft.format(hqrtRobotChatdetails.getParent().getEndttime()));
+            } else {
+            	sqlcondition += " AND a.messagedatetime BETWEEN ? AND ?";
+            	SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            	Calendar cal = Calendar.getInstance();
+                cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
+                Date beginOfDate = cal.getTime();
+            	paramList.add(ft.format(beginOfDate));
+            	Calendar calendar2 = Calendar.getInstance();
+            	calendar2.set(calendar2.get(Calendar.YEAR), calendar2.get(Calendar.MONTH), calendar2.get(Calendar.DAY_OF_MONTH),
+            	        23, 59, 59);
+            	Date endOfDate = calendar2.getTime();
+            	paramList.add(ft.format(endOfDate));
+            	
+            }
+            if (StringUtils.isNotBlank(sqlcondition)) {
+            	sqlcondition = sqlcondition.replaceFirst(" AND", "");
+            	sqlcondition  = " where" + sqlcondition;
+            }
+            // 该语句仅仅为了查询当前条件下有多少符合条件的数据
+            sql += sqlcondition;
+            MultiDBUtils md = MultiDBUtils.get(Global.getConfig("datasourcename"));
+            List<HqrtRobotChatdetails> detailsList = md.queryList(sql, HqrtRobotChatdetails.class, paramList.toArray());
+    		for (int i = 0; i < detailsList.size(); i++) {
+    			detailsList.get(i).setOrdernumber(i+1);
+    		}
+            new ExportExcel("机器人拦截明细日志", HqrtRobotChatdetails.class).setDataList(detailsList).write(response, fileName).dispose();
+    		j.setSuccess(true);
+    		j.setMsg("导出成功！");
+    		return j;
+		} catch (Exception e) {
+			j.setSuccess(false);
+			j.setMsg("导出机器人拦截明细日志记录失败！失败信息："+e.getMessage());
+		}
+			return j;
+    }
+
+}
